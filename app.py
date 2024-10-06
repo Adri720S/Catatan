39feb59f1a3ae68a68d6017a6c8595941ed07a95
Adri720S <adrisopiana3@gmail.com>
2024-10-06 14:15:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyOm1F/zkZ4e68EbLq5TyNhH",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
